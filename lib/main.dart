3012cade4fea89d08b7767dc1da6f61cc289a16d
dev-barro <bardra10@gmail.com>
2024-03-12 01:44:39 +0100

    HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,26 +11,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("OrientaApp"),
-            ),
-            body: const Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  " OrientaApp",
-                  style: TextStyle(fontSize: 24,
-                   fontFamily: 'Poppins'),
-                ),
-                Text(
-                  "L'Appli guide pour reussir son orientation poste secondaire",
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                )
-              ],
-            ))));
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
+}
+
+//deuxième page sur laquelle seront placées les universités public et privéé burkina Faso
+class EventPage extends StatelessWidget {
+  const EventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("OrientaApp"),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                " OrientaApp",
+                style: TextStyle(fontSize: 24, fontFamily: 'Poppins'),
+              ),
+              Text(
+                "L'Appli guide pour reussir son orientation poste secondaire",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ));
+  }
+
+  void onPressed() {}
 }
