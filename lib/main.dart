3a5c7fc78a0287f03c0936fb0faf7108d33ed5bb
dev-barro <bardra10@gmail.com>
2024-03-16 12:27:44 +0100

    post gestion de animation des boutons de la barre de navigation

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,58 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyApp> {
+  // declaration d'un espace pour stocker l'index de la page actuelle
+  int _currentIndex = 0;
+  // la methode permetant de modifier la l'index de ma page
+  setCurrentIndex(int index) {
+    // on met à jour l'interface avec setState
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "CampusOrientApp",
+            style: TextStyle(
+                fontFamily: 'Poppins', fontSize: 15, color: Colors.white),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: const HomePage(),
+        // debut de la creation des boutons de navigation
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex, // propriétée indique la page courante
+
+          onTap: (index) => setCurrentIndex(
+              index), //cette propriété va me permettre de changer de page lorsque clique sur les boutton de navaigation
+          // coloration d'item selection  en couleur verte
+          selectedItemColor: Colors.green,
+          // coloration d'item non selection en couleur noire
+          unselectedItemColor: Colors.black,
+          elevation: 3,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit_sharp), label: 'formations'),
+          ],
+        ),
+        //fin de la creation  des bouttons de la navigation
+      ),
     );
   }
 }
+/*
+*/
--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -12,37 +11,41 @@ class _EventPageState extends State<EventPage> {
   final events = [
     {"nom": "UNZ", "intitule": "universite publique", "avatar": "FlutterLogo"},
     {"nom": "ujkz", "intitule": "universite publique", "avatar": "FlutterLogo"},
-    {"nom": "Nazi boni", "intitule": "universite publique", "avatar": "FlutterLogo"}
+    {
+      "nom": "Nazi boni",
+      "intitule": "universite publique",
+      "avatar": "FlutterLogo"
+    }
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:
-            const Text("La liste des universités disponibles au burkina faso"),
-      ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: events.length,
-          itemBuilder: (context, index) {
-            final event = events[index];
-            final nom = event['nom'];
-            final intitule = event['intitule'];
-            final avatar = event['avatar'];
-            return  Card(
-              child: ListTile(
-                leading: FlutterLogo(
-                  size: 50.0,
-                ),
-                title: Text('$nom'),
-                subtitle: Text('$intitule'),
-                trailing: Icon(Icons.more_vert),
+    return Center(
+      child: ListView.builder(
+        itemCount: events.length,
+        itemBuilder: (context, index) {
+          final event = events[index];
+          final nom = event['nom'];
+          final intitule = event['intitule'];
+          final avatar = event['avatar'];
+          return Card(
+            child: ListTile(
+              leading: const FlutterLogo(
+                size: 50.0,
               ),
-            );
-          },
-        ),
+              title: Text('$nom'),
+              subtitle: Text('$intitule'),
+              trailing: const Icon(Icons.more_vert),
+            ),
+          );
+        },
       ),
     );
   }
 }
+
+    /*Scaffold(
+      appBar: AppBar(
+        title:
+            const Text("La liste des universités disponibles au burkina faso"),
+      ),*/
