0c71366b0e948b3ec7ebbff73b6b505ce905c047
dev-barro <bardra10@gmail.com>
2024-03-16 03:19:48 +0100

    dynamisation de la page EventPage

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,8 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
+
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  final events = [
+    {"nom": "UNZ", "intitule": "universite publique", "avatar": "FlutterLogo"},
+    {"nom": "ujkz", "intitule": "universite publique", "avatar": "FlutterLogo"},
+    {"nom": "Nazi boni", "intitule": "universite publique", "avatar": "FlutterLogo"}
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +22,26 @@ class EventPage extends StatelessWidget {
         title:
             const Text("La liste des universités disponibles au burkina faso"),
       ),
-      body: const Center(
-        child: Text("privée comme public "),
+      body: Center(
+        child: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            final event = events[index];
+            final nom = event['nom'];
+            final intitule = event['intitule'];
+            final avatar = event['avatar'];
+            return  Card(
+              child: ListTile(
+                leading: FlutterLogo(
+                  size: 50.0,
+                ),
+                title: Text('$nom'),
+                subtitle: Text('$intitule'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
