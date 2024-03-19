3e6f9414049513f580949af350e4f8d53285e63e
dev-barro <bardra10@gmail.com>
2024-03-19 09:50:01 +0100

    implementation de la page de garde

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:application/pages/event_page.dart';
 import 'package:application/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyWidgetState extends State<MyApp> {
   // declaration d'un espace pour stocker l'index de la page actuelle
   int _currentIndex = 0;
-  // la methode permetant de modifier la l'index de ma page
+  // la methode permetant d'indexer la page
   setCurrentIndex(int index) {
     // on met à jour l'interface avec setState
     setState(() {
@@ -26,25 +27,27 @@ class _MyWidgetState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(
-            "CampusOrientApp",
-            style: TextStyle(
-                fontFamily: 'Poppins', fontSize: 15, color: Colors.white),
-          ),
+          title: [
+            Text("Accueil"),
+            Text("Listes des universités"),
+          ][_currentIndex],
           backgroundColor: Colors.red,
         ),
-        body: const HomePage(),
+        body: const [
+          // permet de changer de page
+          HomePage(),
+          EventPage()
+        ][_currentIndex],
         // debut de la creation des boutons de navigation
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex, // propriétée indique la page courante
 
           onTap: (index) => setCurrentIndex(
               index), //cette propriété va me permettre de changer de page lorsque clique sur les boutton de navaigation
-          // coloration d'item selection  en couleur verte
-          selectedItemColor: Colors.green,
-          // coloration d'item non selection en couleur noire
-          unselectedItemColor: Colors.black,
-          elevation: 3,
+          selectedItemColor:
+              Colors.green, // coloration d'item selection  en couleur verte
+          unselectedItemColor:
+              Colors.black, // coloration d'item non selection en couleur noire
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(
